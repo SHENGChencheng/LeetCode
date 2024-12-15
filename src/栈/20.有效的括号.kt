644a932ec6775b39b059db851bffc52c7122e945
shengchencheng <shengchencheng@foxmail.com>
2024-12-15 22:38:14 +0800

    feat: leetCode 20.有效的括号

---
+++ src/栈/20.有效的括号.kt
@@ -0,0 +1,42 @@
+package 栈
+
+class ValidParentheses {
+    fun isValid(s: String): Boolean {
+        val paris = mapOf(
+            ')' to '(',
+            '}' to '{',
+            ']' to '['
+        )
+        val stack = mutableListOf<Char>()
+        for (char in s) {
+            when (char) {
+                '(', '{', '[' -> stack.add(char)
+                ')', '}', ']' -> if (stack.isEmpty() ||
+                    stack.removeAt(stack.size - 1) != paris[char]) return false
+            }
+        }
+        return stack.isEmpty()
+    }
+}
+
+fun main() {
+    val validator = ValidParentheses()
+
+    val testCases = listOf(
+        "()" to true,
+        "()[]{}" to true,
+        "(]" to false,
+        "([)]" to false,
+        "{[]}" to true,
+        "(" to false,
+        ")(" to false,
+        "{[()()]}" to true,
+        "" to true,
+        "{{{{}}}}" to true
+    )
+
+    for ((input, expected) in testCases) {
+        val result = validator.isValid(input)
+        println("Input: \"$input\" -> Expected: $expected, Result: $result")
+    }
+}
