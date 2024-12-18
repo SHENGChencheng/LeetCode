9b3f169bd2d20c631571b7cdf505c5bd2bcc9b50
shengchencheng <shengchencheng@foxmail.com>
2024-12-18 21:00:00 +0800

    leetcode: 128

---
+++ src/哈希/128.最长连续序列.kt
@@ -0,0 +1,23 @@
+package 哈希
+
+class LongestConsecutive {
+    fun longestConsecutive(nums: IntArray): Int {
+        val numSet = mutableSetOf<Int>()
+        for (num in nums) {
+            numSet.add(num)
+        }
+        var longestConsecutive = 0
+        for (num in numSet) {
+            if (!numSet.contains(num - 1)) {
+                var currentNum = num
+                var length = 1
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum += 1
+                    length++
+                }
+                longestConsecutive = maxOf(longestConsecutive, length)
+            }
+        }
+        return longestConsecutive
+    }
+}
