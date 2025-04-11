32cd975b3ca54450285d4739ba22608b59d50c59
shengchencheng <shengchencheng@foxmail.com>
2025-04-11 23:37:22 +0800

    leetcode: 141

---
+++ src/链表/141.环形链表.kt
@@ -0,0 +1,18 @@
+package 链表
+
+import common.ListNode
+
+class HasCycle {
+    fun hasCycle(head: ListNode?): Boolean {
+        val visited = mutableSetOf<ListNode>()
+        var pos = head
+        while (pos != null) {
+            if (visited.contains(pos)) {
+                return true
+            }
+            visited.add(pos)
+            pos = pos.next
+        }
+        return false
+    }
+}
