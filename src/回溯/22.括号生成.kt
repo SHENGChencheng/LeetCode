575e84efa31e4885bcd16f0ca2a79662f43f784e
shengchencheng <shengchencheng@foxmail.com>
2025-04-13 19:09:06 +0800

    leetcode: 22

---
+++ src/回溯/22.括号生成.kt
@@ -0,0 +1,29 @@
+package 回溯
+
+class generateParenthesis {
+    fun generateParenthesis(n: Int): List<String> {
+        val ans = mutableListOf<String>()
+        backtrack(ans, "", 0, 0, n)
+        return ans
+    }
+
+    fun backtrack(ans: MutableList<String>, cur: String, open: Int, close: Int, max: Int) {
+        if (cur.length == max * 2) {
+            ans.add(cur)
+            return
+        }
+        if (open < max) {
+            backtrack(ans, cur + "(", open + 1, close, max)
+        }
+        if (close < open) {
+            backtrack(ans, cur + ")", open, close + 1, max)
+        }
+    }
+}
+
+fun main() {
+    val n = 3
+    val generateParenthesis = generateParenthesis()
+    val ans = generateParenthesis.generateParenthesis(n)
+    print(ans)
+}
