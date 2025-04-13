abd46c64b756f47a8e935ce9be1cec060f8113fb
shengchencheng <shengchencheng@foxmail.com>
2025-04-13 16:54:50 +0800

    leetcode: 121, 122

---
+++ src/贪心算法/121.买卖股票的最佳时机.kt
@@ -0,0 +1,34 @@
+package 贪心算法
+
+class MaxProfit {
+    fun maxProfit(prices: IntArray): Int {
+        if (prices.isEmpty()) return 0
+        var minPrice = prices[0]
+        var maxProfit = 0
+        for (price in prices) {
+            if (price < minPrice) {
+                minPrice = price
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice
+            }
+        }
+        return maxProfit
+    }
+}
+
+fun main() {
+    val solution = MaxProfit()
+
+    // 示例测试
+    println(solution.maxProfit(intArrayOf(7,1,5,3,6,4))) // 输出: 5
+
+    // 价格持续下跌
+    println(solution.maxProfit(intArrayOf(7,6,4,3,1)))   // 输出: 0
+
+    // 边界测试
+    println(solution.maxProfit(intArrayOf(1)))           // 输出: 0
+    println(solution.maxProfit(intArrayOf()))            // 输出: 0
+
+    // 高价在前的特殊情况
+    println(solution.maxProfit(intArrayOf(3,2,6,5,0,3))) // 输出: 4
+}
---
+++ src/贪心算法/122.买卖股票的最佳时机2.kt
@@ -0,0 +1,14 @@
+package 贪心算法
+
+class MaxProfit2 {
+    fun maxProfit2(prices: IntArray): Int {
+        val dp = Array(prices.size) { IntArray(2)}
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        for (i in 1 until prices.size) {
+            dp[i][0] = maxOf(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            dp[i][1] = maxOf(dp[i - 1][1], dp[i - 1][0] - prices[i])
+        }
+        return dp[prices.size - 1][0]
+    }
+}
