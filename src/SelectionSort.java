32ce12e698000a8b9cf253cc21e4fb33dbe92d7e
shengchencheng <shengchencheng@foxmail.com>
2025-05-06 17:27:53 +0800

    SelectionSort

---
+++ src/SelectionSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            SortArrayJava.swap(arr, i, minIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        sort(arr);
+        System.out.println(Arrays.toString(arr)); // 输出: [11, 12, 22, 25, 64]
+    }
+}
