d5eb4222acff3e2cecf893dab72820e8ac564cf6
shengchencheng <shengchencheng@foxmail.com>
2025-05-06 15:33:26 +0800

    SortArrayJava

---
+++ src/SortArrayJava.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class SortArrayJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int[] ans = sortArray(nums);
+        for (int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    public static void quickSort(int[] nums, int left, int right) {
+        if (left >= right) return;
+        int pivotIndex = partition(nums, left, right);
+        quickSort(nums, left, pivotIndex - 1);
+        quickSort(nums, pivotIndex + 1, right);
+    }
+
+    public static int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left;
+        for (int j = left; j < right; j++) {
+            if (nums[j] < pivot) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, right);
+        return i;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
