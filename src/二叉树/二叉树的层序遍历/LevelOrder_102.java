d149e53f4262fe0179f7baa0692eb0e17b88c06f
shengchencheng <shengchencheng@foxmail.com>
2025-05-07 15:42:00 +0800

    levelOrder

---
+++ src/二叉树/二叉树的层序遍历/LevelOrder_102.java
@@ -0,0 +1,33 @@
+package 二叉树.二叉树的层序遍历;
+
+import common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder_102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.getVal());
+                if (node.getLeft() != null) {
+                    queue.add(node.getLeft());
+                }
+                if (node.getRight() != null) {
+                    queue.add(node.getRight());
+                }
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
+}
