444a1f35c015c14dbe420e7767e6fd13734e0984
shengchencheng <shengchencheng@foxmail.com>
2025-05-11 21:50:51 +0800

    41.缺失的第一个正数

---
+++ src/普通数组/缺失的第一个正数/41.缺失的第一个正数.kt
@@ -0,0 +1,20 @@
+package 普通数组.缺失的第一个正数
+
+import SortArrayJava.swap
+
+class MissingPositive {
+    fun firstMissingPositive(nums: IntArray): Int {
+        val n = nums.size
+        for (i in 0 until n) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1)
+            }
+        }
+        for (i in 0 until n) {
+            if (nums[i] != i + 1) {
+                return i + 1
+            }
+        }
+        return n + 1
+    }
+}
---
+++ src/普通数组/缺失的第一个正数/FirstMissingPositive_41.java
@@ -0,0 +1,25 @@
+package 普通数组.缺失的第一个正数;
+
+public class FirstMissingPositive_41 {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        // 把每个正整数 x 放到 x - 1 的位置上
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i ,nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
