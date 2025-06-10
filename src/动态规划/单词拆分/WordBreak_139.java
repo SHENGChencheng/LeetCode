aa602fe0f298f9f6795cdccd1680f3734cfeeb5e
shengchencheng <shengchencheng@foxmail.com>
2025-06-10 11:12:05 +0800

    wordbreak139

--- src/动态规划/单词拆分/WordBreak_139.java
+++ src/动态规划/单词拆分/WordBreak_139.java
@@ -5,17 +5,18 @@ import java.util.List;
 public class WordBreak_139 {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                // 找拆分点，前缀可以拆分，拆分点之后的单词在字典中
+        boolean[] dp = new boolean[n + 1]; // dp[i] 表示 s[0..i-1] 是否可以拆分
+        dp[0] = true; // 空字符串可以被拆分
+
+        for (int i = 1; i <= n; i++) { // 检查 s[0..i-1] 是否可以拆分
+            for (int j = 0; j < i; j++) { // 尝试所有可能的分割点 j
                 if (dp[j] && wordDict.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
+                    dp[i] = true; // 如果 s[0..j-1] 可以拆分，且 s[j..i-1] 在字典中
+                    break; // 提前终止
                 }
             }
         }
-        return dp[n];
+
+        return dp[n]; // 返回整个字符串是否可以拆分
     }
 }
