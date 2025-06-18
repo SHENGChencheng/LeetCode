eae871d9d99b2681021e287314ca753271463d0f
shengchencheng <shengchencheng@foxmail.com>
2025-06-18 11:13:31 +0800

    majorityelement_169 annotation

--- src/技巧/多数元素/MajorityElement_169.java
+++ src/技巧/多数元素/MajorityElement_169.java
@@ -2,13 +2,14 @@ package 技巧.多数元素;
 
 public class MajorityElement_169 {
     public int majorityElement(int[] nums) {
+        // 摩尔投票法
         int count = 0;
-        int candidate = nums[0];
+        int candidate = nums[0]; // 初始候选为第一个元素
         for (int num : nums) {
             if (count == 0) {
-                candidate = num;
+                candidate = num; // 当计数归零时，更换候选元素
             }
-            count += (num == candidate) ? 1 : -1;
+            count += (num == candidate) ? 1 : -1; // 相同则加分，不同则扣分
         }
         return candidate;
     }
