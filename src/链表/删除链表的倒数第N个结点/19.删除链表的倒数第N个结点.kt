437fb473a2e94b67249da4e1aed3b4674affcdda
shengchencheng <shengchencheng@foxmail.com>
2025-07-07 11:38:29 +0800

    removeNthFromEnd

--- src/链表/删除链表的倒数第N个结点/19.删除链表的倒数第N个结点.kt
+++ src/链表/删除链表的倒数第N个结点/19.删除链表的倒数第N个结点.kt
@@ -4,18 +4,18 @@ import common.ListNode
 
 class RemoveNthFromEnd {
     fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
-        val dummy = ListNode(0);
-        dummy.next = head;
-        var fast: ListNode? = dummy;
-        var slow: ListNode? = dummy;
+        val dummy = ListNode(0)
+        dummy.next = head
+        var fast: ListNode? = dummy
+        var slow = dummy
         for (i in 0 until n + 1) {
             fast = fast?.next
         }
         while (fast != null) {
             fast = fast.next
-            slow = slow?.next
+            slow = slow.next!!
         }
-        slow?.next = slow?.next?.next
+        slow.next = slow.next?.next
         return dummy.next
     }
 }
