5bb44cbcb131712b66cecdcc0f33a2f7d6f97ef6
shengchencheng <shengchencheng@foxmail.com>
2025-07-11 10:27:53 +0800

    两数之和 哈希表解法

--- src/哈希/两数之和/TwoSum_1.java
+++ src/哈希/两数之和/TwoSum_1.java
@@ -1,6 +1,10 @@
 package 哈希.两数之和;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum_1 {
+    // 暴力枚举
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
@@ -9,6 +13,17 @@ public class TwoSum_1 {
                     return new int[]{i, j};
                 }
             }
+        }
+        return null;
+    }
+    // 哈希表
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
         }
         return null;
     }
