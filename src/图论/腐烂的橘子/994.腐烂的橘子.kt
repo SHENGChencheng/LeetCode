ac6701ba2151b14e7cd6aa4cf1630c7818355f52
shengchencheng <shengchencheng@foxmail.com>
2025-07-15 10:43:40 +0800

    腐烂的橘子

--- src/图论/腐烂的橘子/994.腐烂的橘子.kt
+++ src/图论/腐烂的橘子/994.腐烂的橘子.kt
@@ -4,28 +4,28 @@ class OrangeRotting {
     fun orangesRotting(grid: Array<IntArray>): Int {
         val m = grid.size
         val n = grid[0].size
-        val queue = ArrayDeque<Triple<Int, Int, Int>>()
+        val queue = ArrayDeque<IntArray>()
         var freshCount = 0
         for (i in 0 until m) {
             for (j in 0 until n) {
                 when (grid[i][j]) {
-                    2 -> queue.add(Triple(i, j, 0))
                     1 -> freshCount++
+                    2 -> queue.add(intArrayOf(i, j, 0))
                 }
             }
         }
         var minutes = 0
-        val dirs = arrayOf(intArrayOf(0, 1), intArrayOf(0, -1), intArrayOf(-1, 0), intArrayOf(1, 0))
+        val dirs = arrayOf(intArrayOf(1, 0), intArrayOf(-1, 0), intArrayOf(0, 1), intArrayOf(0, -1))
         while (queue.isNotEmpty()) {
-            val (i, j, time) = queue.removeFirst()
-            minutes = time
+            val orange = queue.removeFirst()
+            minutes = orange[2]
             for (dir in dirs) {
-                val nx = i + dir[0]
-                val ny = j + dir[1]
-                if (nx in 0 until m && ny in 0 until n && grid[nx][ny] == 1) {
-                    grid[nx][ny] = 2
+                val i = orange[0] + dir[0]
+                val j = orange[1] + dir[1]
+                if (i in 0 until m && j in 0 until n && grid[i][j] == 1) {
+                    grid[i][j] = 2
                     freshCount--
-                    queue.add(Triple(nx, ny, time + 1))
+                    queue.add(intArrayOf(i, j, minutes + 1))
                 }
             }
         }
