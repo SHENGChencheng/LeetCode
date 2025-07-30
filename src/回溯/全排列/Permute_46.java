6fed4c3fba4adb4ebd072ea74762119db2aadd92
shengchencheng <shengchencheng@foxmail.com>
2025-07-30 16:20:26 +0800

    46全排列

--- src/回溯/全排列/Permute_46.java
+++ src/回溯/全排列/Permute_46.java
@@ -8,21 +8,20 @@ public class Permute_46 {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         boolean[] used = new boolean[nums.length];
-        backtrace(nums, ans, path, used);
+        backtrack(nums, ans, path, used);
         return ans;
     }
 
-    private void backtrace(int[] nums, List<List<Integer>> ans, List<Integer> path, boolean[] used) {
+    private void backtrack(int[] nums, List<List<Integer>> ans, List<Integer> path, boolean[] used) {
         if (path.size() == nums.length) {
-            List<Integer> pathCopy = new ArrayList<>(path);
-            ans.add(pathCopy);
+            ans.add(new ArrayList<>(path));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
             if (used[i]) continue;
             path.add(nums[i]);
             used[i] = true;
-            backtrace(nums, ans, path, used);
+            backtrack(nums, ans, path, used);
             path.removeLast();
             used[i] = false;
         }
