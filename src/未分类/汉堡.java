71bf9e13729d5e97d699e7e5ace84220159dcb9f
shengchencheng <shengchencheng@foxmail.com>
2025-08-03 22:10:04 +0800

    汉堡

---
+++ src/未分类/汉堡.java
@@ -0,0 +1,39 @@
+package 未分类;
+
+import java.util.Scanner;
+
+public class 汉堡 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+        int[] prices = new int[n];
+        for (int i = 0; i < n; i++) {
+            prices[i] = scanner.nextInt();
+        }
+
+        int maxSum = 0;
+        for (int price : prices) {
+            maxSum += price;
+        }
+
+        boolean[] dp = new boolean[maxSum + 1];
+        dp[0] = true;
+
+        for (int price : prices) {
+            for (int i = maxSum; i >= price; i--) {
+                dp[i] = dp[i] || dp[i - price];
+            }
+        }
+
+        int minTotal = Integer.MAX_VALUE;
+        for (int i = x; i <= maxSum; i++) {
+            if (dp[i]) {
+                minTotal = i;
+                break;
+            }
+        }
+
+        System.out.println(minTotal);
+    }
+}
