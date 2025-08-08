c9e8662e88a312822a08b7496fcb3fb161c6bd08
shengchencheng <shengchencheng@foxmail.com>
2025-08-08 11:26:19 +0800

    227 基本计算器

---
+++ src/未分类/计算器.java
@@ -0,0 +1,35 @@
+package 未分类;
+
+import java.util.ArrayDeque;
+
+public class 计算器 {
+    public int calculate(String s) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        char sign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + (s.charAt(i) - '0');
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                if (sign == '+') {
+                    stack.push(num);
+                } else if (sign == '-') {
+                    stack.push(-num);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+                sign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
