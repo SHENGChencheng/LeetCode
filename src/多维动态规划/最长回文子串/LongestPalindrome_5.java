81911f13df0d5eb0d7eb3d78bb467cc867c99f2a
shengchencheng <shengchencheng@foxmail.com>
2025-09-19 11:14:52 +0800

    最长回文子串

--- src/多维动态规划/最长回文子串/LongestPalindrome_5.java
+++ src/多维动态规划/最长回文子串/LongestPalindrome_5.java
@@ -6,13 +6,13 @@ public class LongestPalindrome_5 {
         boolean[][] dp = new boolean[n][n];
         int start = 0;
         int maxLen = 0;
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i <= j; i++) {
-                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
-                    dp[i][j] = true;
-                    if (j - i + 1 > maxLen) {
-                        maxLen = j - i + 1;
-                        start = i;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i - j < 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
                     }
                 }
             }
